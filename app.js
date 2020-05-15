14c87310be2a8f52806f09c9e291bc147cae75d5
jason-nordheim <jason.nordheim@gmail.com>
2020-05-15 12:01:26 -0600

    can now perform basic calculations, still need to fix issue where after a number is entered, the screen is cleared when the next number is pressed

--- app.js
+++ app.js
@@ -1,34 +1,97 @@
 const SCREEN = document.querySelector('#screen');
 const CLEAR = `C`;
 const DIVIDE = `÷`;
-const MULTIPLE = `x`;
+const MULTIPLY = `x`;
 const SUBTRACT = `-`;
 const ADD = `+`;
 const EQUALS = `=`;
 
-
+let numbersEntered = [];
+let operationsEntered = [];
 
 document.addEventListener('DOMContentLoaded', (event) => {
     buttonsContainer = document.querySelector('.buttons');
-    console.log(buttonsContainer)
+
     for (let i = 0; i < buttonsContainer.children.length; i++) {
         const buttonPressed = buttonsContainer.children[i]
-
-        if (buttonPressed.classList.value != "operator") {
-            // button pressed is a number 
-            buttonPressed.addEventListener('click', (event) => {
-                SCREEN.innerHTML = `${SCREEN.innerHTML.toString()}${event.target.innerText}`;
-            })
-        } else {
-            // button pressed is an operator 
-            buttonPressed.addEventListener('click', (event) => {
-                console.log(buttonPressed.innerText)
-            })
-        }
+        buttonPressed.addEventListener('click', calculatorButtonPressed);
     }
     //console.log(buttons.children);
 });
 
+function calculatorButtonPressed(event) {
+    const btnTextValue = event.target.innerText;
+    if (event.target.classList.value != "operator") {
+        // button pressed is a number 
+        SCREEN.innerHTML = `${SCREEN.innerHTML.toString()}${btnTextValue}`;
+    } else {
+        // button pressed is an operator 
+        const operation = btnTextValue;
+        const numberEntered = parseInt(SCREEN.innerText, 10)
+        operationRequested(numberEntered, operation)
+    }
+}
+
+function saveNumberAndClearScreen(number) {
+    numbersEntered.push(number);
+    SCREEN.innerHTML = ``;
+}
+
+function operationRequested(numberEntered, operation) {
+    saveNumberAndClearScreen(numberEntered);
+    console.log("BEFORE", numbersEntered, operationsEntered)
+
+    if (operation == CLEAR) {
+        console.log('CLEAR')
+        numbersEntered = [];
+        operationRequested = [];
+    } else if (operation == EQUALS) {
+        console.log('EQUALS')
+        let result;
+        let calculation = new Calculation(numbersEntered.shift(), numbersEntered.shift());
+        let opToPerform = operationsEntered.shift();
+        switch (opToPerform) {
+            case ADD:
+                result = calculation.add();
+                break;
+            case SUBTRACT:
+                result = calculation.subtract();
+                break;
+            case MULTIPLY:
+                result = calculation.multiply();
+                break;
+            case DIVIDE:
+                result = calculation.divide();
+                break;
+            default:
+                alert('An error occured - Error Code: A');
+        }
+        SCREEN.innerHTML = result;
+    } else {
+        operationsEntered.push(operation);
+    }
+
+
+    console.log("AFTER", numbersEntered, operationsEntered)
+}
+
+
+
 class Calculation {
-    constructor()
+    constructor(value1, value2) {
+        this.v1 = value1;
+        this.v2 = value2;
+    }
+    multiply() {
+        return this.v1 * this.v2;
+    }
+    divide() {
+        return this.v1 / this.v2;
+    }
+    add() {
+        return this.v1 + this.v2;
+    }
+    subtract() {
+        return this.v1 - this.v2;
+    }
 }
