ad349fc02c80224f39242e4e298ae61669debefe
jason-nordheim <jason.nordheim@gmail.com>
2020-05-15 11:15:15 -0600

    added event listeners to all the buttons and setup the number buttons to append integers to the screen when clicked

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-buttons = document.querySelector('.buttons');
+const SCREEN = document.querySelector('#screen');
+const CLEAR = `C`;
+const DIVIDE = `÷`;
+const MULTIPLE = `x`;
+const SUBTRACT = `-`;
+const ADD = `+`;
+const EQUALS = `=`;
 
-console.log(buttons)
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    buttonsContainer = document.querySelector('.buttons');
+    console.log(buttonsContainer)
+    for (let i = 0; i < buttonsContainer.children.length; i++) {
+        const buttonPressed = buttonsContainer.children[i]
+
+        if (buttonPressed.classList.value != "operator") {
+            // button pressed is a number 
+            buttonPressed.addEventListener('click', (event) => {
+                SCREEN.innerHTML = `${SCREEN.innerHTML.toString()}${event.target.innerText}`;
+            })
+        } else {
+            // button pressed is an operator 
+            buttonPressed.addEventListener('click', (event) => {
+                console.log(buttonPressed.innerText)
+            })
+        }
+    }
+    //console.log(buttons.children);
+});
+
+class Calculation {
+    constructor()
+}
