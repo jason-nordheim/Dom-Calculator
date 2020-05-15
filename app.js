ec631164a601285cf7a00067b4cd4c7751d0691e
jason-nordheim <jason.nordheim@gmail.com>
2020-05-15 14:56:59 -0600

    refactored to be object oriented

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const operations = {
-    screen: document.querySelector('#screen'),
-    clear: `C`,
-    divide: `÷`,
-    multiply: `x`,
-    subtract: `-`,
-    add: `+`,
-    equals: `=`,
-    numbersEntered: [],
-    operationsEntered: [],
-    calculationResultDisplayed: false
-}
+// const operations = {
+//     screen: document.querySelector('#screen'),
+//     clear: `C`,
+//     divide: `÷`,
+//     multiply: `x`,
+//     subtract: `-`,
+//     add: `+`,
+//     equals: `=`,
+//     numbersEntered: [],
+//     operationsEntered: [],
+//     calculationResultDisplayed: false
+// }
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    const calculator = new Calculator();
     buttonsContainer = document.querySelector('.buttons');
 
     for (let i = 0; i < buttonsContainer.children.length; i++) {
@@ -19,91 +21,98 @@ document.addEventListener('DOMContentLoaded', () => {
         //console.log(event.target)
         buttonPressed.addEventListener('click', (event) => calculatorButtonPressed(event.target));
     }
-    //console.log(buttons.children);
+
+    function calculatorButtonPressed(target) {
+        const btnTextValue = target.innerText;
+        //console.log(target)
+        if (target.classList.value != "operator") {
+            // button pressed is a number 
+            if (calculator.status.resultDisplayed) calculator.clearDisplay();
+            calculator.appendScreen(btnTextValue);
+        } else {
+            // button pressed is an operator 
+            calculator.enterOperation(parseInt(calculator.screen, 10), btnTextValue);
+        }
+    }
+
 });
 
-function calculatorButtonPressed(target) {
-    const btnTextValue = target.innerText;
-    //console.log(target)
-    if (target.classList.value != "operator") {
-        // button pressed is a number 
-        if (operations.calculationResultDisplayed) resetScreen();
-        operations.screen.innerHTML = `${operations.screen.innerHTML.toString()}${btnTextValue}`;
-    } else {
-        // button pressed is an operator 
-        const operation = btnTextValue;
-        const numberEntered = parseInt(operations.screen.innerText, 10);
-        operationRequested(numberEntered, operation);
+class Calculator {
+    constructor() {
+        this.operations = {
+            clear: `C`,
+            divide: `÷`,
+            multiply: `x`,
+            subtract: `-`,
+            add: `+`,
+            equals: `=`,
+        }
+        this.status = {
+            numbersEntered: [],
+            operationsEntered: [],
+            resultDisplayed: false
+        }
     }
-}
+    get screen() {
+        return document.querySelector('#screen').innerText;;
+    }
+    set screen(value) {
+        //console.log("set_screen", this);
+        document.querySelector('#screen').innerText = value;
+    }
+    appendScreen(value) {
+        //console.log("append_screen", this);
+        this.screen = this.screen + value;
+    }
+    enterNumber(number) {
+        //console.log("enter_number", this);
+        this.status.numbersEntered.push(number);
+    }
+    enterOperation(number, operation) {
+        if (operation == this.operations.clear) this.reset();
+        else if (operation == this.operations.equals) {
+            this.status.numbersEntered.push(number);
+            this.performCalculation();
+        } else {
+            this.status.numbersEntered.push(number);
+            this.status.operationsEntered.push(operation);
+            this.clearDisplay();
+        }
 
-function resetScreen() {
-    operations.screen.innerHTML = ``;
-    operations.calculationResultDisplayed = false;
-}
-
-function saveNumberAndClearScreen(number) {
-    operations.numbersEntered.push(number);
-    operations.screen.innerHTML = ``;
-}
-
-function operationRequested(numberEntered, operation) {
-    saveNumberAndClearScreen(numberEntered);
-    //console.log("BEFORE", numbersEntered, operationsEntered)
-
-    if (operation == operation.clear) { // clear everything 
-        //console.log('CLEAR')
-        operations.numbersEntered = [];
-        operations.operationsEntered = [];
-        operations.screen.innerText = ``;
-    } else if (operation == operations.equals) { // perform calcuation 
-        //console.log('EQUALS')
-        let result;
-        let calculation = new Calculation(
-            operations.numbersEntered.shift(), operations.numbersEntered.shift()
-        );
-        let opToPerform = operations.operationsEntered.shift();
-        switch (opToPerform) {
-            case operations.add:
-                result = calculation.add();
+    }
+    performCalculation() {
+        const num1 = this.status.numbersEntered.shift();
+        const num2 = this.status.numbersEntered.shift();
+        const operation = this.status.operationsEntered.shift();
+        console.log(`perform calculation=> num1:${num1}, num2:${num2}, op:${operation}`)
+        this.status.resultDisplayed = true;
+        switch (operation) {
+            case this.operations.add:
+                this.screen = num1 + num2;
                 break;
-            case operations.subtract:
-                result = calculation.subtract();
+            case this.operations.subtract:
+                this.screen = num1 - num2;
                 break;
-            case operations.multiply:
-                result = calculation.multiply();
+            case this.operations.multiply:
+                this.screen = num1 * num2;
                 break;
-            case operations.divide:
-                result = calculation.divide();
+            case this.operations.divide:
+                this.screen = num1 / num2;
                 break;
             default:
+                this.status.resultDisplayed = false;
                 alert('An error occured - Error Code: A');
         }
-        operations.screen.innerHTML = result;
-        operations.calculationResultDisplayed = true;
-    } else { // 
-        operations.operationsEntered.push(operation);
     }
-    //console.log("AFTER", numbersEntered, operationsEntered)
-}
-
-
-
-class Calculation {
-    constructor(value1, value2) {
-        this.v1 = value1;
-        this.v2 = value2;
+    reset() {
+        // console.log("reset", this);
+        this.clearDisplay();
+        this.status.numbersEntered = [];
+        this.status.operationsEntered = [];
     }
-    multiply() {
-        return this.v1 * this.v2;
-    }
-    divide() {
-        return this.v1 / this.v2;
-    }
-    add() {
-        return this.v1 + this.v2;
-    }
-    subtract() {
-        return this.v1 - this.v2;
+    clearDisplay() {
+        // console.log("clear_display", this);
+        this.screen = ``;
+        this.status.resultDisplayed = false;
     }
 }
